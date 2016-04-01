aa28ba677467e2b33b9bb13af6437b73ebf99b1b
Matthew Newell <mnewell10@gmail.com>
2016-04-01 13:20:47 -0700

    Update readme

--- app/routes/question.js
+++ app/routes/question.js
@@ -1,10 +1,10 @@
-
 import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('question', params.question_id);
-  },actions: {
+  },
+  actions: {
     save(params) {
        var newAnswer = this.store.createRecord('answer', params);
        var question = params.question;
