9f54c176250ad615b9561e05f3fe5054c7da92c5
Matthew Newell <mnewell10@gmail.com>
2016-04-01 10:52:42 -0700

    Add ability to update questions

--- app/components/update-question.js
+++ app/components/update-question.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  updateQuestionForm: false,
+  actions: {
+    updateQuestionForm() {
+      this.set('updateQuestionForm', true);
+    },
+    update(question) {
+      var params = {
+        content: this.get('content'),
+        author: this.get('author'),
+        additionalNotes: this.get('additionalNotes')
+      };
+      this.set('updateQuestionForm', false);
+      this.sendAction('update', question, params);
+    }
+  }
 });
--- app/routes/question.js
+++ app/routes/question.js
@@ -4,5 +4,15 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('question', params.question_id);
-  },
+  },actions: {
+    update(question, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          question.set(key,params[key]);
+        }
+      });
+      question.save();
+      this.transitionTo('question');
+    }
+  }
 });
