df480f44da071ea5fbf85d8f08e12fd60a064e0a
Matthew Newell <mnewell10@gmail.com>
2016-03-31 23:22:06 -0700

    Add question route with dynamic segment for individual question id

--- app/router.js
+++ app/router.js
@@ -9,7 +9,7 @@ Router.map(function() {
   this.route('contact');
   this.route('about');
   this.route('question-list');
-  this.route('question');
+  this.route('question', {path: '/question/:question_id'});
 });
 
 export default Router;
