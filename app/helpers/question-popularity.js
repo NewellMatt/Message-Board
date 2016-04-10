0a616e3dd7216c72efff05f70113f7d810bf5ce5
Matthew Newell <mnewell10@gmail.com>
2016-04-10 16:44:12 -0700

    Add comment glyphicon to pop vote between 1 and 5

--- app/helpers/question-popularity.js
+++ app/helpers/question-popularity.js
@@ -6,6 +6,9 @@ export function questionPopularity(params/*, hash*/) {
     if(popularity.get('length') >= 5) {
       return Ember.String.htmlSafe('<span class="glyphicon glyphicon-fire"></span>');
     }
+    if(popularity.get('length') >= 1 && popularity.get('length') < 5) {
+      return Ember.String.htmlSafe('<span class="glyphicon glyphicon-comment"></span>');
+    }
   }
 
 export default Ember.Helper.helper(questionPopularity);
